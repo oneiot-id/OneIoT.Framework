c1d6f13d480f10f075108b7a8dbf5465a701c96a
vincentkenutama <vincentkenutama.2021@student.uny.ac.id>
2024-11-15 00:50:57 +0700

    first commit

---
+++ OneIoT.Framework/OneIoT.Framework/Graphic/Window.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OneIoT.Framework.Graphic
+{
+    public class Window : GameWindow
+    {
+        public Window(int width, int height, string title) : base(GameWindowSettings.Default, 
+            new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+            
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+    }
+    
+    
+    
+}
+
+
---
+++ OneIoT.Framework/OneIoT.Framework/Program.cs
@@ -0,0 +1,12 @@
+﻿using OneIoT.Framework.Graphic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Window window = new Window(200, 200, "test");
+
+        window.Run();
+        
+    }
+}
